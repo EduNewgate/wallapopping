b94e16e75db14a79ecf0631c42865483fa62ecfd
eduardo.moreno.93 <eduardo.moreno.93@gmail.com>
2024-04-08 01:15:47 +0200

    Add node-cache and subcategories.

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const TelegramBot = require('node-telegram-bot-api');
+const https = require('https');
+const NodeCache = require( "node-cache" );
 const { CONSTANTS } = require('./token');
 const { URLS } = require('./urls');
 const bot = new TelegramBot(CONSTANTS.TLGRM_TOKEN, {polling: true});
-const https = require('https');
+const myCache = new NodeCache();
+
+let searchs = [];
 
 bot.setMyCommands([
     { command: "start", description: "Iniciar bot." },
@@ -11,7 +15,7 @@ bot.setMyCommands([
 ]);
 
 bot.onText('/start', (msg) => {
-    bot.sendMessage(msg.chat.id, "Bienvenido/a " + msg.from.username + " " + msg.from.id + " " + msg.from.first_name + " " + msg.from.last_name);
+    bot.sendMessage(msg.chat.id, "Bienvenido/a " + msg.from.first_name);
 });
 
 bot.onText('/help', (msg) => {
@@ -51,40 +55,62 @@ bot.on('callback_query', (callbackQuery) => {
         chat_id: msg.chat.id,
         message_id: msg.message_id
     }
-    if (action === 'addSearch') {
-        const reqOptions = {
-            hostname: URLS.API_HOST,
-            path: URLS.API_BASE_ENDPOINT + URLS.CATEGORIES_ENDPOINT,
-            headers: {
-                "Accept-Language": "es-ES"
+    if (action === 'addSearch' || action === 'editSearch' || action === 'deleteSearch') {
+        if (action === 'addSearch') {
+            if (!myCache.get("categories")) {
+                const reqOptions = {
+                    hostname: URLS.API_HOST,
+                    path: URLS.API_BASE_ENDPOINT + URLS.CATEGORIES_ENDPOINT,
+                    headers: {
+                        "Accept-Language": "es-ES"
+                    }
+                }
+                https.get(reqOptions, (res) => {
+                    let data = [];
+                    res.on('data', (chunk) => {
+                        data.push(chunk);
+                    });
+                    res.on('end', () => {
+                        const categories = JSON.parse(Buffer.concat(data).toString());
+                        myCache.set("categories", categories.categories);
+                        const options = {
+                            reply_markup: {
+                                resize_keyboard: true,
+                                one_time_keyboard: true,
+                                inline_keyboard: buildCatButtons(categories.categories)
+                            }
+                        }
+                        bot.sendMessage(callbackQuery.message.chat.id, 'Seleccione una categoría:', options);
+                    });
+                }).on('error', err => {
+                    console.log('ERROR: ' + err)
+                });
             }
         }
-        https.get(reqOptions, (res) => {
-            let data = [];
-            res.on('data', (chunk) => {
-                data.push(chunk);
-            });
-            res.on('end', () => {
-                const categories = JSON.parse(Buffer.concat(data).toString());
-                let catButtons = buildCatButtons(categories.categories);
+        if (action === 'editSearch') {
+            bot.sendMessage(options.chat_id, 'Búsqueda editada!');
+        }
+        if (action === 'deleteSearch') {
+            bot.sendMessage(options.chat_id, 'Búsqueda eliminada!');
+        }
+    } else {
+        if (myCache.get("categories")) {
+            const categories = myCache.get("categories");
+            const catId = +action;
+            const subcategories = categories.find((category) => category.id === catId).subcategories;
+            if (subcategories) {
                 const options = {
                     reply_markup: {
                         resize_keyboard: true,
                         one_time_keyboard: true,
-                        inline_keyboard: catButtons
+                        inline_keyboard: buildCatButtons(subcategories)
                     }
                 }
-                bot.sendMessage(callbackQuery.message.chat.id, 'Seleccione una categoría:', options);
-            });
-        }).on('error', err => {
-            console.log('ERROR: ' + err)
-        });
-    }
-    if (action === 'editSearch') {
-        bot.sendMessage(options.chat_id, 'Búsqueda editada!');
-    }
-    if (action === 'deleteSearch') {
-        bot.sendMessage(options.chat_id, 'Búsqueda eliminada!');
+                bot.sendMessage(callbackQuery.message.chat.id, 'Seleccione una subcategoría:', options);
+            } else {
+
+            }
+        }
     }
 });
 
@@ -99,7 +125,7 @@ function buildCatButtons(categories) {
             buttonsMatrix.push(catBtn);
             catBtn = [];
         }
-        catBtn.push({text: element.name, callback_data: element.name});
+        catBtn.push({text: element.name, callback_data: element.id.toString()});
     }
     return buttonsMatrix;
 }
